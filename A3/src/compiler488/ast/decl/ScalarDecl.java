86aa6a130c0a153e4db2791ead5cbce149297c50
Jathu Satkunarajah <jathu.satkunarajah@gmail.com>
2017-03-01 20:35:20 -0500

    Work on Declaration subclasses

--- A3/src/compiler488/ast/decl/ScalarDecl.java
+++ A3/src/compiler488/ast/decl/ScalarDecl.java
@@ -3,12 +3,17 @@ package compiler488.ast.decl;
 import java.util.ArrayList;
 
 import compiler488.symbol.SymbolTable;
+import compiler488.ast.type.Type;
 
 /**
  * Represents the declaration of a simple variable.
  */
 
 public class ScalarDecl extends Declaration {
+	
+	public ScalarDecl(String name, Type type) {
+		super(name, type);
+	}
 
 	/**
 	 * Returns a string describing the name and type of the object being
