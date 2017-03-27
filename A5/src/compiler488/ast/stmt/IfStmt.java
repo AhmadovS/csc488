257015432624b8c5cfefed9d92e0881eeb948325
mrsoong <stevensong1994@hotmail.com>
2017-03-27 13:18:22 -0400

    Reverted bad commit "Fixed a bug with if statements"

--- A5/src/compiler488/ast/stmt/IfStmt.java
+++ A5/src/compiler488/ast/stmt/IfStmt.java
@@ -120,16 +120,20 @@ public class IfStmt extends Stmt {
 		this.getWhenTrue().doCodeGen(writer);
 
 		if(this.whenFalse != null) {
-			// The if statement has a non-empty else clause
+			// Exit the conditional statement
+			writer.add(Machine.PUSH, Machine.UNDEFINED);
+			short exitAddr = writer.getPrevAddr();
+			writer.add(Machine.BR);
+			
 			// Update false address
 			writer.replace(exitFalseAddr, writer.getNextAddr());
 			
 			// Execute false expression
 			this.getWhenFalse().doCodeGen(writer);
+
+			// Update exit address
+			writer.replace(exitAddr, writer.getNextAddr());
 		} else {
-			// The if statement doesn't have an else clause
-			// So just let the first condition cause a branch 
-			// to the end of the loop
 			writer.replace(exitFalseAddr, writer.getNextAddr());
 		}
 
