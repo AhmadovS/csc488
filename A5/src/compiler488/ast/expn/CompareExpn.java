7b0685635dfe729c68ec6a111436ddf47df7c19d
mrsoong <stevensong1994@hotmail.com>
2017-03-28 19:00:53 -0400

    CompareExpn no longer uses double evaluation for comparisons

--- A5/src/compiler488/ast/expn/CompareExpn.java
+++ A5/src/compiler488/ast/expn/CompareExpn.java
@@ -63,43 +63,49 @@ public class CompareExpn extends BinaryExpn {
 		String op = this.getOpSymbol();
 		Expn operand1 = getLeft();
 		Expn operand2 = getRight();
-
+		
 		// Order the memory load based on operator, since there is no greater than operator.
-		if(op.equals(OPSYMBOL.GREATER_THAN) || op.equals(OPSYMBOL.GREATER_THAN_EQUALS)) {
+		if(op.equals(OPSYMBOL.GREATER_THAN) || op.equals(OPSYMBOL.LESS_THAN_EQUALS)) {
 		    // switches the order of the operands.
 			operand1 = getRight();
 			operand2 = getLeft();
 		}
-
+		
 		// Emits code to evaluate the operands
 		operand1.doCodeGen(writer);
 		operand2.doCodeGen(writer);
-
-		// Stack :: oprand1 -> operand2
-		if (op.equals(OPSYMBOL.GREATER_THAN_EQUALS) || op.equals(OPSYMBOL.LESS_THAN_EQUALS)) {
-
-			// Performs LT operation, and then emits code
-			// that calculates the operands again, so we can do
-			// equality comparison
+		
+		if (op.equals(OPSYMBOL.GREATER_THAN_EQUALS)) {
+			// Performs a NOT(operand1 < operand2) operation
             writer.add(Machine.LT);
-            operand1.doCodeGen(writer);
-            operand2.doCodeGen(writer);
-            writer.add(Machine.EQ);
-            writer.add(Machine.OR);
-
-        } else if (op.equals(OPSYMBOL.GREATER_THAN) || op.equals(OPSYMBOL.LESS_THAN)) {
+			// Negate the value of (operand1 < operand2)
+			writer.add(Machine.PUSH, Machine.MACHINE_FALSE);
+			writer.add(Machine.EQ);
+        } 
+		else if (op.equals(OPSYMBOL.LESS_THAN_EQUALS)) {
+			// Performs a NOT (operand2 < operand1) operation
+			// where NOT is performed by performing an equality op with
+			// a MACHINE_FALSE value
+            writer.add(Machine.LT);
+			// Negate the value of (operand2 < operand1)
+			writer.add(Machine.PUSH, Machine.MACHINE_FALSE);
+			writer.add(Machine.EQ);
+		}
+		else if (op.equals(OPSYMBOL.GREATER_THAN)){
+			// Perform a (operand2 < operand1) operation
+			writer.add(Machine.LT);
+		} 
+		else if (op.equals(OPSYMBOL.LESS_THAN)) {
 		    writer.add(Machine.LT);
 		} else if (op.equals(OPSYMBOL.EQUALS)) {
 			writer.add(Machine.EQ);
 		} else if (op.equals(OPSYMBOL.NOT_EQUALS)) {
 		    writer.add(Machine.EQ);
 		    // Stack :: result of equality testing (either 1 or 0).
-		    writer.add(Machine.PUSH, 1);
-		    writer.add(Machine.SWAP);
-		    // Stack :: 1 -> equailty-result
-		    writer.add(Machine.SUB);
+			// Negate the result of teh equality operation
+		    writer.add(Machine.PUSH, Machine.MACHINE_FALSE);
+		    writer.add(Machine.EQ);
 		}
-
 
 	}
 }
